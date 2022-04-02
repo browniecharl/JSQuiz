b5112ec7cffda37e9cd8d2009aecd0330f01f55e
browniecharl <99853094+browniecharl@users.noreply.github.com>
2022-04-01 20:06:24 -0400

    initial setup

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+var questions = [
+    
+    {
+        questionContent: "Arrays in JavaScript can be used to store ______.",
+        choices: [
+        "1. numbers and strings",
+        "2. other arrays",
+        "3. booleans",
+        "4. all of the above",
+      ],
+      answer: "4. all of the above",
+    },
+    {
+        questionContent:
+        "Which of the following is a statement that can be used to terminate a loop, switch or label statement?",
+        choices: ["1. break", "2. stop", "3. halt", "4. exit"],
+      answer: "1. break",
+    },
+    {
+        questionContent:
+        "String values must be enclosed within _____ when being assigned to variables.",
+        choices: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
+      answer: "3. quotes",
+    },
+    {
+        questionContent: "Commonly used data types DO NOT include:",
+        choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        answer: "3. alerts",
+      },
+    {
+        questionContent:
+        "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: [
+        "1. JavaScript",
+        "2. terminal/bash",
+        "3. for loops",
+        "4. console.log",
+      ],
+      answer: "4. console.log",
+    },    
+  ];
